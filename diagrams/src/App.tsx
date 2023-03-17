cd3a6ed0bf6e23b43534e1d03237113eec33b413
chevonfdz <86297885+chevonfdz@users.noreply.github.com>
2023-03-17 15:21:35 +0530

    Updated vscode Extension according to the EI-Tool

--- diagrams/src/App.tsx
+++ diagrams/src/App.tsx
@@ -1,8 +1,91 @@
+import React, { useCallback, useState } from 'react';
 import { vscode } from "./utilities/vscode";
+import styled from 'styled-components';
 import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
-import "./App.css";
+import './App.css';
+
+import { FileNode, root } from './data';
+
+
+type Props = {
+  node: FileNode,
+  add?: Function
+}
+
+const File = (props: Props) => {
+
+
+  const [showChildren, setShowChildren] = useState<boolean>(false);
+
+  const [addedNode, setAddedNode] = useState<FileNode>();
+
+  const addFun = (data: FileNode) => {
+    setAddedNode(
+      () => {
+        console.log(data);
+        return data;
+      }
+
+    )
+  }
+
+  const handleClick = useCallback(() => {
+
+
+    //todo: need to pass this to next level to get data
+    props.node.selection ?
+      props.add && props.add(() => {
+        console.log(props.node);
+        return props.node;
+      })
+
+      :
+      setShowChildren(!showChildren);
+
+
+  }, [props, showChildren])
+
+  return (
+    <div>
+      <TextButton onClick={() => props.node.selection ? handleClick() : null} >
+
+        {props.node.id}
+        {!props.node.selection ?
+          <PlusButton onClick={() =>
+            handleClick()
+
+          }>{showChildren ? "-" : "+"}</PlusButton> : ""
+        }
+      </TextButton>
+      <DisplayDiv>
+        {
+          showChildren && (props.node.children ?? []).map((node: FileNode) => (
+            <>
+              {!node.selection ? <>
+                <BoxDiv>
+                  {addedNode ?
+                    //todo: need to call addFun
+                    <File node={{ ...addedNode }} add={() => addFun(props.node)} />
+                    : <File node={{ ...node }} />}
+
+                </BoxDiv>
+              </>
+                : <>
+                  <SelectList>
+                    <File node={{ ...node }} add={() => addFun(props.node)} />
+                  </SelectList>
+                </>}
+
+            </>
+          ))
+        }
+      </DisplayDiv>
+    </div>
+  )
+}
 
 function App() {
+  
   function resourcesPanel() {
     vscode.postMessage(
       {
@@ -12,14 +95,53 @@ function App() {
     );
   }
 
-
-
   return (
-    <main>
-      <h1>Hello World!</h1>
-      <VSCodeButton onClick={resourcesPanel}>Resources</VSCodeButton>
-    </main>
+    <div style={{ marginLeft: 15 }}>
+
+      {root.dataList && root.dataList.map(data => {
+        return (
+          <BoxDiv>
+            <File node={{ ...data }} />
+            <VSCodeButton onClick={resourcesPanel}>Resources</VSCodeButton>
+          </BoxDiv>
+          
+          )
+      }
+      )}
+
+    </div>
   );
 }
 
 export default App;
+
+const PlusButton = styled.div`
+  cursor: pointer;
+  display: inline-block;
+`;
+
+const TextButton = styled.div`
+
+`;
+
+const DisplayDiv = styled.div`
+  position: relative;
+  display: block;
+  flexDirection: column;
+  left: 25;
+  // border-left: 1px solid;
+  paddingLeft: 15; `;
+
+
+const BoxDiv = styled.div`
+  border: 2px solid;
+  padding:10px;
+  margin-top:50px;
+  margin-right: 15px;
+
+  `;
+
+const SelectList = styled.div`
+  display:inline-block;
+  border: 1px solid;
+`;
--- diagrams/src/utilities/vscode.ts
+++ diagrams/src/utilities/vscode.ts
@@ -1,14 +1,6 @@
-import type { WebviewApi } from "vscode-webview";
+import { WebviewApi } from "vscode-webview";
 
-/**
- * A utility wrapper around the acquireVsCodeApi() function, which enables
- * message passing and state management between the webview and extension
- * contexts.
- *
- * This utility also enables webview code to be run in a web browser-based
- * dev server by using native web browser features that mock the functionality
- * enabled by acquireVsCodeApi.
- */
+
 class VSCodeAPIWrapper {
   private readonly vsCodeApi: WebviewApi<unknown> | undefined;
 
@@ -20,14 +12,7 @@ class VSCodeAPIWrapper {
     }
   }
 
-  /**
-   * Post a message (i.e. send arbitrary data) to the owner of the webview.
-   *
-   * @remarks When running webview code inside a web browser, postMessage will instead
-   * log the given message to the console.
-   *
-   * @param message Abitrary data (must be JSON serializable) to send to the extension context.
-   */
+
   public postMessage(message: unknown) {
     if (this.vsCodeApi) {
       this.vsCodeApi.postMessage(message);
@@ -36,14 +21,7 @@ class VSCodeAPIWrapper {
     }
   }
 
-  /**
-   * Get the persistent state stored for this webview.
-   *
-   * @remarks When running webview source code inside a web browser, getState will retrieve state
-   * from local storage (https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage).
-   *
-   * @return The current state or `undefined` if no state has been set.
-   */
+
   public getState(): unknown | undefined {
     if (this.vsCodeApi) {
       return this.vsCodeApi.getState();
@@ -53,17 +31,7 @@ class VSCodeAPIWrapper {
     }
   }
 
-  /**
-   * Set the persistent state stored for this webview.
-   *
-   * @remarks When running webview source code inside a web browser, setState will set the given
-   * state using local storage (https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage).
-   *
-   * @param newState New persisted state. This must be a JSON serializable object. Can be retrieved
-   * using {@link getState}.
-   *
-   * @return The new state.
-   */
+
   public setState<T extends unknown | undefined>(newState: T): T {
     if (this.vsCodeApi) {
       return this.vsCodeApi.setState(newState);
@@ -74,5 +42,4 @@ class VSCodeAPIWrapper {
   }
 }
 
-// Exports class singleton to prevent multiple invocations of acquireVsCodeApi.
 export const vscode = new VSCodeAPIWrapper();
