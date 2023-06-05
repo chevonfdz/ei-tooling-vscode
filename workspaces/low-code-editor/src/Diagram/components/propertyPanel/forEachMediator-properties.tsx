81ef28a85aefba75964e8248b20b9f67877e3cba
RukshanFdo <rukshanfernando234@gmail.com>
2023-06-05 23:23:45 +0530

    Convert Form type to Function format in propertyPanel and update the Styles-css file

--- workspaces/low-code-editor/src/Diagram/components/propertyPanel/forEachMediator-properties.tsx
+++ workspaces/low-code-editor/src/Diagram/components/propertyPanel/forEachMediator-properties.tsx
@@ -1,26 +1,131 @@
-import React, { Component } from 'react'
-import { Col, Form, Modal, Row, OverlayTrigger, Tooltip, Button } from 'react-bootstrap'
+/**
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+import React, { Component, useContext, useState } from "react";
+import {
+  Col,
+  Form,
+  Modal,
+  Row,
+  OverlayTrigger,
+  Tooltip,
+  Button,
+} from "react-bootstrap";
+import "./style.scss";
 import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  CompletionResponse,
+  DiagramEditorLangClientInterface,
+  SnippetCompletionResponse,
+  TextEdit,
+} from "@wso2-ei/low-code-editor-commons";
+import {
+  applyChange,
+  getCompletion,
+  getSnippetCompletion,
+} from "../../../DiagramGenerator/generatorUtil";
+import { Context as DiagramContext } from "../../../Contexts";
 
-type Props = {};
+type Props = {
+  // langClient: DiagramEditorLangClientInterface;
+  textDocumentUrl: string;
+  textDocumentFsPath: string;
+  previousComponentStartPosition: number;
+  textEdit?: TextEdit;
+};
 type State = {
-    selectedSequenceType: string;
+    forEachID: string;
+    forEachExpression: string;
+  description: string;
+    sequenceType: string;
+    sequenceKey: string;
+    sequenceName: string;
 };
-
-export default class ForEachMediatorProperty  extends Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            selectedSequenceType: "Anonymous",
-        };
-}
-handleSequenceTypeSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    this.setState({ selectedSequenceType: event.target.value });
-};
-  render() {
-    const { selectedSequenceType } = this.state;
-
+export function ForEachMediatorProperty(props: Props) {
+  const {
+    textDocumentUrl,
+    textDocumentFsPath,
+    previousComponentStartPosition,
+    textEdit,
+  } = props;
+ const [forEachID, setForEachID] = useState<string>("");
+    const [forEachExpression, setForEachExpression] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+    const [selectedSequenceType, setSelectedSequenceType] = useState<string>("Anonymous");
+    const [sequenceKey, setSequenceKey] = useState<string>("");
+    const [sequenceName, setSequenceName] = useState<string>("");
+  const {
+    api: {
+      ls: { getDiagramEditorLangClient },
+    },
+  } = useContext(DiagramContext);
+  const handleForEachID = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForEachID(event.target.value);
+    };
+    const handleForEachExpression = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForEachExpression(event.target.value);
+    };
+  const handleDescription = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(event.target.value);
+  };
+    const handleSequenceTypeSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedSequenceType(event.target.value);
+    };
+    const handleSequenceKey = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSequenceKey(event.target.value);
+    };
+    const handleSequenceName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSequenceName(event.target.value);
+    };
+  const handleSubmit = async () => {
+    if (!getDiagramEditorLangClient || !textEdit) {
+      return [];
+    }
+    const langClient = await getDiagramEditorLangClient();
+    let snippetCompletionResponse: SnippetCompletionResponse =
+      await getSnippetCompletion(
+        forEachID,
+        forEachExpression,
+        description,
+        selectedSequenceType,
+        sequenceKey,
+        // sequenceName,
+        langClient
+      );
+    textEdit.newText = snippetCompletionResponse.snippet;
+    await modifyTextOnComponentSelection(
+      textDocumentUrl,
+      textDocumentFsPath,
+      textEdit,
+      previousComponentStartPosition,
+      langClient
+    );
+  };
+  const handleCancelClick = async () => {
+   setForEachID("");
+    setForEachExpression("");
+    setDescription("");
+    setSelectedSequenceType("Anonymous");
+    setSequenceKey("");
+    setSequenceName("");
+  };
     return (
         <>
             <Modal.Header>
@@ -35,17 +140,27 @@ handleSequenceTypeSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) =
                     <Form>
                         <Form.Group>    
                             <Form.Label className="ForEachID">ForEach ID</Form.Label>
-                            <Form.Control type="text" placeholder="eg: ForEach ID" />
+                            <Form.Control type="text" placeholder="eg: ForEach ID" value={forEachID} onChange={handleForEachID} />
                             <Form.Label className="ForEachExpression">ForEach Expression</Form.Label>
                             {/* When a user clicks this textbox, the Expression Selector Model appears.*/}
-                            <Form.Control type="text" placeholder="eg: Iterate Expression" />
-                            <Form.Label className='mt-2 mb-0'>Description</Form.Label>
-                                <OverlayTrigger placement="right" overlay={<Tooltip id="help-tooltip">Default description</Tooltip>}>
-                                    <span style={{ marginLeft: "10px", cursor: "pointer" }}>
-                                        <FontAwesomeIcon icon={faQuestionCircle} size="sm" />
-                                    </span>
-                                </OverlayTrigger>
-                            <Form.Control as="textarea" placeholder='eg: None'/> 
+                            <Form.Control type="text" readOnly value={forEachExpression} onChange={handleForEachExpression} />
+                            <Form.Label className="Description">Description</Form.Label>
+              <OverlayTrigger
+                placement="right"
+                overlay={
+                  <Tooltip id="help-tooltip">Default description</Tooltip>
+                }
+              >
+                <span style={{ marginLeft: "10px", cursor: "pointer" }}>
+                  <FontAwesomeIcon icon={faQuestionCircle} size="sm" />
+                </span>
+              </OverlayTrigger>
+              <Form.Control
+                as="textarea"
+                value={description}
+                onChange={handleDescription}
+                placeholder="eg: None"
+              />
                         </Form.Group>
                     </Form>
                 </Row> 
@@ -57,7 +172,7 @@ handleSequenceTypeSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) =
                     <Form>
                         <Form.Group>
                             <Form.Label className="SequenceType">Sequence Type</Form.Label>
-                            <Form.Select value={selectedSequenceType} onChange={this.handleSequenceTypeSelectChange}>
+                            <Form.Select value={selectedSequenceType} onChange={handleSequenceTypeSelectChange}>
                                 <option value="Anonymous">Anonymous</option>
                                 <option value="RegistryReference">Registry Reference</option>
                                 <option value="NamedReference">Named Reference</option>
@@ -66,20 +181,44 @@ handleSequenceTypeSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) =
                                 <>
                                     <Form.Label className="SequenceKey">Sequence Key</Form.Label>
                                     {/* When a user clicks this textbox, the Resource KeyModel appears.*/}
-                                    <Form.Control type="text" readOnly />
+                                    <Form.Control type="text" readOnly value={sequenceKey} onChange={handleSequenceKey} />
                                 </>
                             )}
                             {selectedSequenceType === "NamedReference" && (
                                 <>
                                     <Form.Label className="SequenceName">Sequence Name</Form.Label>
-                                    <Form.Control type="text" placeholder="eg: Sequence Name" />
+                                    <Form.Control type="text" placeholder="eg: Sequence Name" value={sequenceName} onChange={handleSequenceName} />
                                 </>
                             )}
                         </Form.Group>
                     </Form>
                 </Row>  
             </Modal.Body>
-        </> 
-    )
-  }
+            <Modal.Footer>
+        <div className="footer-button-container">
+          <Button id="primary-button" onClick={handleSubmit}>
+            Save
+          </Button>
+          <Button id="secondary-button" onClick={handleCancelClick}>
+            Cancel
+          </Button>
+        </div>
+      </Modal.Footer>
+    </>
+  );
+}
+async function modifyTextOnComponentSelection(
+  url: string,
+  fsPath: string,
+  text: TextEdit,
+  previousComponentStartPosition: number,
+  langClient: any
+) {
+  await applyChange(
+    url,
+    fsPath,
+    text,
+    previousComponentStartPosition,
+    langClient
+  );
 }
--- workspaces/low-code-editor/src/Diagram/components/propertyPanel/oAuthMediator-properties.tsx
+++ workspaces/low-code-editor/src/Diagram/components/propertyPanel/oAuthMediator-properties.tsx
@@ -1,20 +1,117 @@
-import React, { Component } from 'react'
-import { Col, Form, Modal, Row, OverlayTrigger, Tooltip, Button } from 'react-bootstrap'
+/**
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+import React, { Component, useContext, useState } from "react";
+import {
+  Col,
+  Form,
+  Modal,
+  Row,
+  OverlayTrigger,
+  Tooltip,
+  Button,
+} from "react-bootstrap";
+import "./style.scss";
 import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  CompletionResponse,
+  DiagramEditorLangClientInterface,
+  SnippetCompletionResponse,
+  TextEdit,
+} from "@wso2-ei/low-code-editor-commons";
+import {
+  applyChange,
+  getCompletion,
+  getSnippetCompletion,
+} from "../../../DiagramGenerator/generatorUtil";
+import { Context as DiagramContext } from "../../../Contexts";
 
-type Props = {};
-type State = {};
-
-export default class OAuthMediatorProperty extends Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {};
-} 
-    
-  render() {
-    const { } = this.state;
-
+type Props = {
+  // langClient: DiagramEditorLangClientInterface;
+  textDocumentUrl: string;
+  textDocumentFsPath: string;
+  previousComponentStartPosition: number;
+  textEdit?: TextEdit;
+};
+type State = {
+    remoteServiceUrl: string;
+    username: string;
+    password: string;
+  description: string;
+};
+export function OAuthMediatorProperty(props: Props) {
+  const {
+    textDocumentUrl,
+    textDocumentFsPath,
+    previousComponentStartPosition,
+    textEdit,
+  } = props;
+    const [remoteServiceUrl, setRemoteServiceUrl] = useState<string>("");
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const {
+    api: {
+      ls: { getDiagramEditorLangClient },
+    },
+  } = useContext(DiagramContext);
+    const handleRemoteServiceUrl = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRemoteServiceUrl(event.target.value);
+    };
+    const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value);
+    };
+    const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    };
+  const handleDescription = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(event.target.value);
+  };
+  const handleSubmit = async () => {
+    if (!getDiagramEditorLangClient || !textEdit) {
+      return [];
+    }
+    // const langClient = await getDiagramEditorLangClient();
+    // let snippetCompletionResponse: SnippetCompletionResponse =
+    //   await getSnippetCompletion(
+    //     remoteServiceUrl
+    //     username,
+    //     password,
+    //     description,
+    //     langClient
+    //   );
+    // textEdit.newText = snippetCompletionResponse.snippet;
+    // await modifyTextOnComponentSelection(
+    //   textDocumentUrl,
+    //   textDocumentFsPath,
+    //   textEdit,
+    //   previousComponentStartPosition,
+    //   langClient
+    // );
+  };
+  const handleCancelClick = async () => {
+    setRemoteServiceUrl("");
+    setUsername("");
+    setPassword("");
+    setDescription("");
+  };
     return (
         <>
             <Modal.Header>
@@ -29,7 +126,7 @@ export default class OAuthMediatorProperty extends Component<Props, State> {
                     <Form>
                         <Form.Group>
                             <Form.Label className="RemoteServiceUrl">Remote Service Url</Form.Label>
-                            <Form.Control type="text" placeholder="eg: Remote Service Url" />
+                            <Form.Control type="text" placeholder="eg: Remote Service Url" value={remoteServiceUrl} onChange={handleRemoteServiceUrl} />
                             <br />
                             <Row className='mb-4'>
                                 <Modal.Title className='text-secondary'>
@@ -38,19 +135,24 @@ export default class OAuthMediatorProperty extends Component<Props, State> {
                                 <Form>
                                     <Form.Group>
                                         <Form.Label className="Username">Username</Form.Label>
-                                        <Form.Control type="text" placeholder="eg: Username" />
+                                        <Form.Control type="text" placeholder="eg: Username" value={username} onChange={handleUsername} />
                                         <Form.Label className="Password">Password</Form.Label>
-                                        <Form.Control type="text" placeholder="eg: Password" />
+                                        <Form.Control type="text" placeholder="eg: Password" value={password} onChange={handlePassword} />
                                     </Form.Group>
                                 </Form>
                             </Row>
-                            <Form.Label className='mt-2 mb-0'>Description</Form.Label>
+                            <Form.Label className='description'>Description</Form.Label>
                                 <OverlayTrigger placement="right" overlay={<Tooltip id="help-tooltip">Default description</Tooltip>}>
                                     <span style={{ marginLeft: "10px", cursor: "pointer" }}>
                                     <FontAwesomeIcon icon={faQuestionCircle} size="sm" />
                                     </span>
                                 </OverlayTrigger>
-                            <Form.Control as="textarea" placeholder='eg: None'/>                            
+                                <Form.Control
+                as="textarea"
+                value={description}
+                onChange={handleDescription}
+                placeholder="eg: None"
+              />                          
                         </Form.Group>
                     </Form>
                 </Row>          
@@ -58,4 +160,18 @@ export default class OAuthMediatorProperty extends Component<Props, State> {
         </> 
     )
   }
+async function modifyTextOnComponentSelection(
+  url: string,
+  fsPath: string,
+  text: TextEdit,
+  previousComponentStartPosition: number,
+  langClient: any
+) {
+  await applyChange(
+    url,
+    fsPath,
+    text,
+    previousComponentStartPosition,
+    langClient
+  );
 }
